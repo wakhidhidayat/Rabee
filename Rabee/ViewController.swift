095f1dfe3080456a3551fc40b3405e8d29950536
Wahid Hidayat <wahid.hidayat@samudrainovasi.co.id>
2023-07-17 22:26:55 +0700

    first commit

---
+++ Rabee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rabee
+//
+//  Created by Wahid Hidayat on 17/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xcodegen generate; open Rabee.xcodeproj
