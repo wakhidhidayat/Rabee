ea15c46a08b4ff1edb1353b2420f89fef74da5a0
Wahid Hidayat <wahid.hidayat@samudrainovasi.co.id>
2023-07-29 15:07:18 +0700

    added selected state in coloe button

--- Rabee/Module/Select Color/SelectColorView.swift
+++ Rabee/Module/Select Color/SelectColorView.swift
@@ -38,6 +38,27 @@ class SelectColorView: UIView {
         return view
     }()
     
+    private let topColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+
+    private let bottomColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    private let vstackTopBottomColor: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 0
+        stack.distribution = .fillEqually
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
     private let pickColorView: UIView = {
         let view = UIView()
         view.clipsToBounds = true
@@ -81,7 +102,7 @@ class SelectColorView: UIView {
         let blurView = UIView()
         blurView.clipsToBounds = true
         blurView.layer.cornerRadius = 16
-        blurView.backgroundColor = .whiteColor.withAlphaComponent(0.7)
+        blurView.backgroundColor = .peach.withAlphaComponent(0.30)
         blurView.translatesAutoresizingMaskIntoConstraints = false
         return blurView
     }()
@@ -122,6 +143,7 @@ class SelectColorView: UIView {
         addSubview(colorView)
         addSubview(nextButton)
         
+        colorView.addSubview(vstackTopBottomColor)
         colorView.addSubview(colorTitleLabel)
         colorView.addSubview(descriptionLabel)
         colorView.addSubview(blurView)
@@ -130,7 +152,10 @@ class SelectColorView: UIView {
         vStack.addArrangedSubview(colorTitleLabel)
         vStack.addArrangedSubview(descriptionLabel)
         
-        reloadData()
+        vstackTopBottomColor.addArrangedSubview(topColorView)
+        vstackTopBottomColor.addArrangedSubview(bottomColorView)
+        
+        reloadData(for: nil)
         
         blurView.addSubview(stackView)
         
@@ -150,7 +175,7 @@ class SelectColorView: UIView {
             colorView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
             colorView.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -16),
             
-            vStack.centerYAnchor.constraint(equalTo: colorView.centerYAnchor, constant: -48),
+            vStack.centerYAnchor.constraint(equalTo: vstackTopBottomColor.centerYAnchor),
             vStack.leadingAnchor.constraint(equalTo: colorView.leadingAnchor, constant: 32),
             vStack.trailingAnchor.constraint(equalTo: colorView.trailingAnchor, constant: -32),
             
@@ -158,6 +183,10 @@ class SelectColorView: UIView {
             blurView.bottomAnchor.constraint(equalTo: colorView.bottomAnchor),
             blurView.trailingAnchor.constraint(equalTo: colorView.trailingAnchor),
             blurView.heightAnchor.constraint(equalToConstant: 180),
+            
+            vstackTopBottomColor.topAnchor.constraint(equalTo: colorView.topAnchor),
+            vstackTopBottomColor.bottomAnchor.constraint(equalTo: blurView.topAnchor),
+            vstackTopBottomColor.widthAnchor.constraint(equalTo: colorView.widthAnchor),
             
             stackView.topAnchor.constraint(equalTo: blurView.topAnchor, constant: 18),
             stackView.leadingAnchor.constraint(equalTo: blurView.leadingAnchor, constant: 13),
@@ -168,41 +197,84 @@ class SelectColorView: UIView {
     
     @objc private func colorButtonTapped(_ sender: ColorButton) {
         UIView.animate(withDuration: 1, delay: .zero) { [weak self] in
-            self?.colorView.backgroundColor = sender.backgroundColor
             switch sender.buttonColor {
             case .blackAndWhite:
-                self?.colorTitleLabel.textColor = .textBlack
+                self?.topColorView.backgroundColor = .textBlack
+                self?.bottomColorView.backgroundColor = .whiteColor
+                self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .textBlack
+                self?.colorTitleLabel.text = "Monokromatik"
+                self?.descriptionLabel.text = "Menonjolkan tampilan yang elegan, komtemporer, dan klasik. Kamu juga bisa memasukkan warna lain sebagai aksen seperti rose gold dan warna pastel yang bisa menonjolkan kesan lembut, romantis namun tentunya tetap terlihat indah."
             case .black:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Hitam"
+                self?.descriptionLabel.text = "Warna hitam memiliki arti yang elegan. Serta memiliki banyak kombinasi warna yang bisa digabungkan yaitu merah, gold, silver, agar lebih terkesan sexy dan elegan."
             case .white:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .textBlack
                 self?.descriptionLabel.textColor = .textBlack
+                self?.colorTitleLabel.text = "Putih"
+                self?.descriptionLabel.text = "Warna ini memiliki kesan yang simpel dan romantis. Warna putih juga memiliki arti positif, yaitu kesucian  dan ketulusan."
             case .yellow:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .textBlack
                 self?.descriptionLabel.textColor = .textBlack
+                self?.colorTitleLabel.text = "Kuning"
+                self?.descriptionLabel.text = "Warna Kuning memiliki arti yang gembira, muda, kebahagiaan, dan optimisme. Warna ini akan cocok jika dikombinasikan dengan warna biru dan hijau."
             case .orange:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Oren"
+                self?.descriptionLabel.text = "Berarti sebuah keceriaan, kesuksesan, kegembiraan, dan sebuah antusiasme. Sering dipadukan dengan warna-warna lembut agar terlihat lebih cerah."
             case .red:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Merah"
+                self?.descriptionLabel.text = "Memiliki nuansa dengan arti sesuatu yang kuat, semangat, suka cita, dan percaya diri. Untuk dekorasi pernikahan, dapat dipadukan dengan warna gold agar terlihat lebih megah dan mewah."
             case .purple:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Ungu"
+                self?.descriptionLabel.text = "Warna ini memiliki arti kesetiaan, kekuatan yang luar biasa, dan empati. Warna ungu sering dipadukan dengan silver dan juga pink."
             case .magenta:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Magenta"
+                self?.descriptionLabel.text = "Umumnya warna ini memiliki nuansa yang melambangkan elegansi, kemewahan, dan keanggunan."
             case .green:
-                self?.colorTitleLabel.textColor = .whiteColor
-                self?.descriptionLabel.textColor = .whiteColor
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
+                self?.colorTitleLabel.textColor = .textBlack
+                self?.descriptionLabel.textColor = .textBlack
+                self?.colorTitleLabel.text = "Hijau"
+                self?.descriptionLabel.text = "Memiliki arti ketenangan, kesejukan dan kesegaran."
             case .teal:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Teal"
+                self?.descriptionLabel.text = "Warna teal melambangkan ketenangan dan keseimbangan mental serta spiritual."
             case .blue:
+                self?.topColorView.backgroundColor = sender.backgroundColor
+                self?.bottomColorView.backgroundColor = sender.backgroundColor
                 self?.colorTitleLabel.textColor = .whiteColor
                 self?.descriptionLabel.textColor = .whiteColor
+                self?.colorTitleLabel.text = "Biru"
+                self?.descriptionLabel.text = "Memiliki arti yang menggambarkan ketenangan seperti halnya alam. Untuk biru muda sering dipadukan dengan baby pink. Namun bisa juga dipadukan warna putih, gold, silver, atau bahkan kuning agar terlihat lebih fresh."
             }
         }
         didColorBtnTapped?(sender)
@@ -212,7 +284,7 @@ class SelectColorView: UIView {
         didNextBtnTapped?()
     }
     
-    func reloadData() {
+    func reloadData(for data: UserSelectedModel?) {
         stackView.removeAllArrangedSubviews()
         
         let buttonsPerRow = 4
@@ -235,10 +307,28 @@ class SelectColorView: UIView {
                 let colorButton = ColorButton()
                 colorButton.buttonColor = ColorFilter.allCases[buttonIndex]
                 colorButton.addTarget(self, action: #selector(colorButtonTapped), for: .touchUpInside)
+                if let pallets = data?.colors {
+                    for pallete in pallets {
+                        if pallete.color == colorButton.buttonColor && pallete.usersId.count > 1 {
+                            colorButton.userSelected = .couple
+                        } else if pallete.color == colorButton.buttonColor && pallete.usersId.count == 1 {
+                            colorButton.userSelected = .woman
+                        }
+                    }
+                }
                 
                 rowStackView.addArrangedSubview(colorButton)
                 buttonIndex += 1
             }
         }
+    }
+}
+
+extension UIImage {
+    func resized(to newSize: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: newSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
